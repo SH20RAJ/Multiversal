13cff9b37c33fe9cc7c67783cd114fa346a761a9
sh20raj <shaswatraj3@gmail.com>
2024-08-17 11:15:15 +0530

    Manifest and favicon icons - Manifest - Updated the favicon with a new image

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,9 +8,17 @@ export const metadata = {
   description: "Your Story Starts Here and ends here",
 };
 
+let script = `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-YSDTRCV5B2');`;
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-YSDTRCV5B2"></script>
+      <script dangerouslySetInnerHTML={{ __html: script }} />
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
